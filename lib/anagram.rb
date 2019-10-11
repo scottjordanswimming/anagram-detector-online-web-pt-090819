328c9e1c91e545e8bb1691ab7205e9b23bb169ac
Scott Jordan <scottjordanswimming@gmail.com>
2019-10-11 15:45:52 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+  
+  attr_accessor :word
+  
+  
+  
+  def initialization(word)
+    @word = word
+end
+
+def match(array)
+  if array == @word
+  array
+end
+end 
+
+end
