f840b5c7b8ab171cf38985972dd72c68ff2c363f
Scott Jordan <scottjordanswimming@gmail.com>
2019-10-11 16:28:24 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,27 @@
-# Your code goes here!
+
+require 'pry'
+class Anagram
+  
+  attr_accessor :word
+  
+  
+  
+  def initialize(word)
+    @word = word
+end
+
+def match(array)
+  arr = []
+  x = word.split("")
+  x.sort
+  array.each do |el| 
+  
+    if el.split("").sort == x.sort 
+      arr << el
+    end
+end
+arr
+end 
+
+
+end
